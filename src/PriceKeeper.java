13b15c90e8d3afc019f93009a45e83d007e1f3b6
tomasfrancisco <mail@tomasfrancisco.com>
2015-10-09 10:04:48 +0100

    PriceKeeper structure; created lib directory

--- src/PriceKeeper.java
+++ src/PriceKeeper.java
@@ -1,2 +1,28 @@
-public class PriceKeeper {
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.naming.Context;
+
+public class PriceKeeper implements MessageListener {
+    Context ctx;
+
+    public PriceKeeper() {
+
+    }
+
+    @Override
+    public void onMessage(Message message) {
+
+    }
+
+    public void onClose() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
