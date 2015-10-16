f313b95a5ae4bc27200d20db9224dc8437eaf760
Daniel Amaral <amaral94daniel@gmail.com>
2015-10-16 17:21:28 +0100

    Summary creator com validação.

--- src/HTMLSummaryCreator.java
+++ src/HTMLSummaryCreator.java
@@ -1,6 +1,13 @@
+import org.xml.sax.SAXException;
 import java.io.*;
 import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.*;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.XMLConstants;
+
 
 public class HTMLSummaryCreator {
     public static void main(String[] args)
@@ -9,9 +16,27 @@ public class HTMLSummaryCreator {
         {
             TransformerFactory tFactory = TransformerFactory.newInstance();
 
+            //Sources
+            Source xsdDoc = new StreamSource("src/xmlSchema.xsd");
             Source xslDoc = new StreamSource("src/htmlSummaryCreator.xsl");
             Source xmlDoc = new StreamSource("src/SavedSmartphones.xml");
 
+            //Validacao
+
+            SchemaFactory sFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+            Schema schema = sFactory.newSchema(xsdDoc);
+            Validator validator = schema.newValidator();
+
+            try {
+                validator.validate(xmlDoc);
+                System.out.println(xmlDoc.getSystemId() + " is valid");
+            } catch (SAXException e) {
+                System.out.println(xmlDoc.getSystemId() + " is NOT valid");
+                System.out.println("Reason: " + e.getLocalizedMessage());
+            }
+
+            //Criação do HTML
             String outputFileName = "src/SavedSmartphonesTable.html";
             OutputStream htmlFile = new FileOutputStream(outputFileName);
 
